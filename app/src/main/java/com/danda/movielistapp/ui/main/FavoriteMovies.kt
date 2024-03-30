2da5dbe9b80bd1696fbceaf1c57112c848ff5619
Danda <dandagaharu77@gmail.com>
2024-03-30 21:32:46 +0700

    update app icons and stuff

--- app/src/main/java/com/danda/movielistapp/ui/main/FavoriteMovies.kt
+++ app/src/main/java/com/danda/movielistapp/ui/main/FavoriteMovies.kt
@@ -2,17 +2,19 @@ package com.danda.movielistapp.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.danda.movielistapp.databinding.FavoriteMoviesBinding
 import com.danda.movielistapp.helper.FavViewModelFactory
 import com.danda.movielistapp.offlinedb.MovieModel
+import com.danda.movielistapp.offlinedb.repository.MovieRepo
 import com.danda.movielistapp.ui.detail.MovieViewModel
 
 class FavoriteMovies : AppCompatActivity() {
     private lateinit var binding: FavoriteMoviesBinding
-    private lateinit var viewModel: MovieViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +32,15 @@ class FavoriteMovies : AppCompatActivity() {
         binding.backButton.setOnClickListener {
             onBackPressed()
         }
+
+        val movieRepo = MovieRepo(application)
+        movieRepo.getAllMovie().observe(this, Observer { movies ->
+            if (movies.isEmpty()) {
+                binding.tvNoFavorite.visibility = View.VISIBLE
+            } else {
+                binding.tvNoFavorite.visibility = View.GONE
+            }
+        })
     }
     private fun obtainViewModel(activity: AppCompatActivity): FavViewModel {
         val factory = FavViewModelFactory.getInstance(activity.application)
